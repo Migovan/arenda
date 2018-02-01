d8e8a9f8bffe4af000cf9e2a09c819b1b2c70951
Migovan <mr.migovan@mail.ru>
2018-02-01 19:02:42 +0500

    fix scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,28 @@
-let gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
     browserSync = require('browser-sync');
 
-gulp.task('sass', function () {
-  return gulp.src('app/sass/**/*.scss')
+let sassPath = 'app/sass/**/*.scss'
+
+gulp.task('sass', function() {
+  return gulp.src(sassPath)
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(browserSync.reload({stream: true}))
 });
- 
+
 gulp.task('browser-sync', function () { 
-    browserSync({ 
-        server: { 
-            baseDir: 'app' 
-        },
-        notify: false 
-    });
+  browserSync({ 
+    server: { 
+      baseDir: 'app',
+      routes : {
+        '/node_modules' : './node_modules'
+      }
+    },
+    notify: false 
+  });
 });
 
-  
-  gulp.task('watch', gulp.parallel('browser-sync','sass'), function () {
-  gulp.watch('app/sass/**/*.scss', gulp.series('sass'));
-  gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/js/**/*.js', browserSync.reload);
-});
-
+gulp.task('watch', gulp.parallel('browser-sync', function() {
+  gulp.watch(sassPath, gulp.series('sass'))
+}))
