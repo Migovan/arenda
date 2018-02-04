923083cede304652f1f2218e7ccd8cb3f6829911
Migovan <mr.migovan@mail.ru>
2018-02-05 03:02:54 +0500

    add change ptice

---
+++ app/js/change-price.js
@@ -0,0 +1,19 @@
+
+let $price = $('.product__price > p');
+  let $productCharacteristics = $('.product__characteristic');
+
+  $productCharacteristics.each(function() {
+    $(this).click(function() {
+      let $this = $(this);
+      $productCharacteristics.find('p');// снял классы со всех характеристик
+      $this.find('p');// ставит на текущую характеристику
+
+      $price.html($this.data('price'))
+
+      $productCharacteristics.find('div').removeClass('product__checkbox-black'); // снял классы со всех характеристик
+      $this.find('div').addClass('product__checkbox-black'); // ставит на текущую характеристику
+    })
+  })
+
+
+
